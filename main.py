c9e0312524977aa9def910a470a3ef0ab24f0285
ndkvin <andika.kavin@gmail.com>
2024-12-31 16:00:29 +0700

    Initial Commit

---
+++ main.py
@@ -0,0 +1,14 @@
+import streamlit as st
+import pandas as pd
+
+@st.cache_data
+def load_data():
+    return pd.read_csv('./data/data.csv', index_col=0)
+
+st.set_page_config(page_title='Prediksi Biaya Perkalanan Haji', layout='wide', page_icon="📊")
+
+st.sidebar.title('Sidebar')
+
+st.title('Prediksi Biaya Perkalanan Haji')
+st.table(load_data())
+
